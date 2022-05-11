d02d4ed0c290b8d4bff3555dc2152ba340eca33c
KyuGG <zverolovz@mail.ru>
2022-05-11 23:55:16 +0300

    remove trash

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from json import load
-from os import listdir, remove
+from os import listdir
 from shutil import copyfile
 from sys import platform
 
@@ -7,17 +7,11 @@ with open('settings.json') as settings:
     csgo_path = load(settings)
 csgo_path = csgo_path.get('csgo_path')
 
-
-overlay = '/csgo/materials/overlay/' if platform == 'linux' else '\csgo\materials\overlay\\'  
+overlay = '/csgo/materials/overlay/' if platform == 'linux' else '\csgo\materials\overlay\\'
 overlay_path = csgo_path + overlay
 overlay_files = listdir(overlay_path)
-vtf_names = []
 for file in overlay_files:
     if file.startswith('cybershoke') and file.endswith('.vtf'):
-        vtf_names.append(file)
-        remove(overlay_path + file)
-
-for vtf_name in vtf_names:
-    copyfile('cybershoke.vtf', overlay_path + vtf_name)
+        copyfile('cybershoke.vtf', overlay_path + file)
 
 input('Скрипт выполнен. Нажмите ENTER')
